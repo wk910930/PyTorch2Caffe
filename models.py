f5cb763105465c14fcb4b8535c99942361782e83
wangkun <wangkun@sensetime.com>
2017-07-19 11:00:24 +0800

    Add models.py

---
+++ models.py
@@ -0,0 +1,54 @@
+"""
+Several CNNs for demo
+"""
+
+import torch.nn as nn
+
+class AlexNet(nn.Module):
+    """
+    https://github.com/pytorch/vision/blob/master/torchvision/models/alexnet.py
+    """
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
+
+class ToyNet(nn.Module):
+    """
+    A toy neural network with just an affine operation: y = Wx + b
+    """
+    def __init__(self, num_classes=1000):
+        super(ToyNet, self).__init__()
+        self.layer = nn.Linear(10, num_classes)
+
+    def forward(self, x):
+        out = self.layer(x)
+        return out
